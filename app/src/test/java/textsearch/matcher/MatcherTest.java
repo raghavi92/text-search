5f850ebf064dd5a134be11749d500545f71b815c
raghavi <raghavi92@gmail.com>
2024-08-16 12:19:33 +0530

    update test for Matcher

--- app/src/test/java/textsearch/matcher/MatcherTest.java
+++ app/src/test/java/textsearch/matcher/MatcherTest.java
@@ -13,30 +13,30 @@ import org.junit.jupiter.api.Test;
 import textsearch.ingestion.TextLine;
 
 public class MatcherTest {
-    Matcher matcher = new Matcher(
-            List.of(new TextLine(0, "hello tom"), new TextLine(1, "how are you tom?"),
-                    new TextLine(2, "how are things?")),
-            List.of("tom", "how"));
+        Matcher matcher = new Matcher(
+                        List.of(new TextLine(0, "hello tom"), new TextLine(1, "how are you tom?"),
+                                        new TextLine(2, "how are things?")),
+                        List.of("tom", "how"));
 
-    @Test
-    void testMatch() {
+        @Test
+        void testMatch() {
 
-        Map<String, List<TextLineMatch>> m = matcher.compute();
+                Map<String, List<TextLineMatch>> m = matcher.compute();
 
-        Assertions.assertEquals(2, m.size());
+                Assertions.assertEquals(2, m.size());
 
-        m.get("tom");
+                m.get("tom");
 
-        assertEquals(m.get("tom").size(), 2);
-        assertEquals(m.get("how").size(), 2);
+                assertEquals(m.get("tom").size(), 2);
+                assertEquals(m.get("how").size(), 2);
 
-        assertThat(m.get("tom"), containsInAnyOrder(
-                new TextLineMatch(0, 6),
-                new TextLineMatch(1, 12)));
+                assertThat(m.get("tom"), containsInAnyOrder(
+                                new TextLineMatch(0, 6),
+                                new TextLineMatch(1, 12)));
 
-        assertThat(m.get("how"), containsInAnyOrder(
-                new TextLineMatch(1, 0),
-                new TextLineMatch(2, 0)));
+                assertThat(m.get("how"), containsInAnyOrder(
+                                new TextLineMatch(1, 0),
+                                new TextLineMatch(2, 0)));
 
-    }
+        }
 }
